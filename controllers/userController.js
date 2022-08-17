6f2f0b5e1ab40b98f41097988734160a3a9872fd
ethernetwins <111263766+ethernetwins@users.noreply.github.com>
2022-08-17 14:43:18 +0800

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,6 @@
 "use strict";
 const UserDB = require('../models/UserDB');
 const User = require('../models/User');
-const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken')
-var secret = "sckey";
 
 var userDB = new UserDB();
 
@@ -45,44 +42,6 @@ function deleteUser(request, respond){
     });
 };
 
-function updateUser(request, respond){
-    var username = request.body.user_id;
-    var first_name = request.body.first_name;
-    var token = request.body.token;
-    try {
-        var decoded = jwt.verify(token,secret);
-        userDB.updateUser(username, first_name, function(error, result){
-            if (error){
-                respond.json(error);
-            }
-            else{
-                respond.json(result);
-            }
-        });
-    } catch (error) {
-        respond.json({result:"invaild"});
-    }
-    
-}
 
-function loginUser(request, respond){
-    var username = request.body.user_id;
-    var password = request.body.password;
-    userDB.loginUser(username, function (error, result) {
-        if (error){
-            respond.json(error);
-        }
-        else{
-            const hash = result[0].password;
-            var flag = bcrypt.compareSync(password, hash);
-            if (flag) {
-                var token = jwt.sign(username,secret);
-                respond.json({result:token});
-            } else {
-                respond.json({result:"invaild"});
-            }
-        }
-    })
-};
 
-module.exports = {getAllUser, addUser, deleteUser, updateUser, loginUser};
+module.exports = {getAllUser, addUser, deleteUser};
