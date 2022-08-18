f3662bb4c6c469c559a4c02a87450281c989eafa
ethernetwins <111263766+ethernetwins@users.noreply.github.com>
2022-08-18 20:55:32 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -9,3 +9,17 @@ var starBWImage = 'images/grey_star.jpg';
 var starImage = 'images/gold_star.png';
 var rating = 0;
 var translate_url =  'https://pbifhdqua4.execute-api.us-east-1.amazonaws.com/test/translate'
+
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
