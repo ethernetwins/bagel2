59fac84f5d6ce31033fd2a1cb8b0c50ad16f15b2
ethernetwins <111263766+ethernetwins@users.noreply.github.com>
2022-08-18 22:57:24 +0800

    Update restaurant.js

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -71,16 +71,31 @@ function showRestaurantDetails(element) {
 }
 
 function translateText() {
+    var trans = document.getElementById("txtName").value;
+
     var request = new XMLHttpRequest();
     request.open('POST', translate_url, true);
-    var translate = JSON.parse(request.responseText);
-    console.log(translate);
     
     var lblName = document.getElementById("lblName");
-    var trans = document.getElementById("txtName").value;
-    console.log(trans)
+    
 
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function () {
+        console.log("new comment sent");
+        translate = getTranslate();
+    };
 
     request.send(JSON.stringify(trans));
     lblName.innerHTML = translate;
+}
+
+function getTranslate(){
+    var request = new XMLHttpRequest();
+    request.open('GET', translate_url, true);
+    request.onload = function () {
+        trans = JSON.parse(request.responseText);
+        console.log(trans);
+    };
+
+    request.send();
 }
