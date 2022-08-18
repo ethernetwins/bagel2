ff0bfe8e4554bbc316634c21b28928d3098162ed
ethernetwins <111263766+ethernetwins@users.noreply.github.com>
2022-08-18 20:31:13 +0800

    Update restaurant.js

--- public/js/restaurant.js
+++ public/js/restaurant.js
@@ -87,19 +87,3 @@ function translateText() {
     request.send(JSON.stringify(request));
     lblName.innerHTML = restaurant_array;
 }
-
-//import { boto3 } from "boto3" as boto3;
-//var translate_client;
-//translate_client = boto3.client("translate");
-
-//function lambda_handler(event, context) {
-    //var review_text, translate_response;
-    //review_text = event["text"];
-    //translate_response = translate_client.translate_text({
-        //"Text": review_text,
-        //"SourceLanguageCode": "auto",
-        //"TargetLanguageCode": "hi"
-    //});
-    //console.log(translate_response);
-    //return translate_response["TranslatedText"];
-//}
